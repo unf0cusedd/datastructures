8c551f43b19e98233cff9f99f0414726073ecdea
unf0cusedd <kozin.2016@yandex.ru>
2025-09-10 15:53:18 +0300

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,36 @@
+
+class Node:
+    def __init__(self, value, next_node):  
+        self.value = value
+        self.next = next_node
+
+class LinkedList:
+    def __init__(self):  
+        self.start = None 
+        self.length = 0
+    
+    def add(self, value):  
+        self.start = Node(value, self.start)
+        self.length += 1
+
+    def print(self): 
+        while self.start != None: 
+            print(self.start.value)
+            self.start = self.start.next
+        
+
+    def pop(self):
+        p = self.start.value
+        new_start = self.start.next 
+        self.start = new_start
+        return p 
+    
+    def empty(self): 
+        if self.start == None: 
+            print("Stack is empty")
+            return True 
+    
+    def __len__(self): 
+        return self.length
+
+
